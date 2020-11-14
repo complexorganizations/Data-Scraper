f1dfe4d591aa4a74ac070c6ad8426c0d6d59fdef
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-11-14 17:47:13 -0500

    Update backend.go

--- backend.go
+++ backend.go
@@ -631,7 +631,7 @@ func scraper(siteMap *Scraping, parent string) map[string]interface{} {
 					data[job.startURL] = job.linkOutput
 
 					switch config.Export {
-					case "XML":
+					case "xml":
 						output, err := xml.MarshalIndent(data, "", " ")
 						if err != nil {
 							if config.Log {
@@ -647,7 +647,7 @@ func scraper(siteMap *Scraping, parent string) map[string]interface{} {
 
 						_ = ioutil.WriteFile(outputFile, output, 0644)
 
-					case "CSV":
+					case "csv":
 						csvFile, err := os.OpenFile(outputFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 						if err != nil {
 							if config.Log {
@@ -675,7 +675,7 @@ func scraper(siteMap *Scraping, parent string) map[string]interface{} {
 						csvWriter.Flush()
 
 						csvFile.Close()
-					case "JSON":
+					case "json":
 						output, err := json.MarshalIndent(data, "", " ")
 						if err != nil {
 							if config.Log {
