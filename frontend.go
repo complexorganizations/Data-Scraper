b6dca3a9e3a78a6ffa6704170ac5d285feffa886
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-11-17 13:26:44 -0500

    Update frontend.go

--- frontend.go
+++ frontend.go
@@ -495,7 +495,7 @@ func uiEditSelector(index int) string {
 						<select id="map_type">
 							<option value="Text" ` + IfThenElse(el.Type == "SelectorText", `selected="selected"`, "") + `>Text</option>
 							<option value="Link" ` + IfThenElse(el.Type == "SelectorLink", `selected="selected"`, "") + `>Link</option>
-							<option value="Popup Link" ` + IfThenElse(el.Type == "SelectorPopupLink", `selected="selected"`, "") + `>Popup link</option>
+							<option value="Popup Link" ` + IfThenElse(el.Type == "SelectorPopupLink", `selected="selected"`, "") + `>Popup Link</option>
 							<option value="Image" ` + IfThenElse(el.Type == "SelectorImage", `selected="selected"`, "") + `>Image</option>
 							<option value="Table" ` + IfThenElse(el.Type == "table", `selected="selected"`, "") + `>Table</option>
 							<option value="Element attribute" ` + IfThenElse(el.Type == "SelectorImage", `selected="selected"`, "") + `>Element attribute</option>
@@ -503,7 +503,7 @@ func uiEditSelector(index int) string {
 							<option value="Element" ` + IfThenElse(el.Type == "element", `selected="selected"`, "") + `>Element</option>
 							<option value="Element scroll down" ` + IfThenElse(el.Type == "scroll", `selected="selected"`, "") + `>Element scroll down</option>
 							<option value="Grouped" ` + IfThenElse(el.Type == "grouped", `selected="selected"`, "") + `>Grouped</option>
-							<option value="Sitemap.xml Links" ` + IfThenElse(el.Type == "xml", `selected="selected"`, "") + `>Sitemap.xml links</option>
+							<option value="Sitemap.xml links" ` + IfThenElse(el.Type == "xml", `selected="selected"`, "") + `>Sitemap.xml links</option>
 						</select>
 					</tr>
 					<tr>
