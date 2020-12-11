497d421a6a2c455ebb68748065d6b64100f8f15d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-12-11 08:57:15 -0500

    Update backend.go

--- backend.go
+++ backend.go
@@ -50,6 +50,16 @@ type selectors struct {
 	Regex            string   `json:"regex,omitempty"`
 	Delay            *int     `json:"delay,omitempty"`
 	ExtractAttribute string   `json:"extractAttribute,omitempty"`
+	//Special Attribute data	
+	AttributeName      string   `json:"attributeName,omitempty"`	
+	HeaderRowSelector  string   `json:"headerRowSelector,omitempty"`	
+	DataRowsSelector   string   `json:"dataRowsSelector,omitempty"`	
+	SitemapURLs        []string `json:"sitemapUrls,omitempty"`	
+	FoundUrlRegex      string   `json:"foundUrlRegex,omitempty"`	
+	MinimumPriority    *float64 `json:"minimumPriority,omitempty"`	
+	ClickSelector      string   `json:"clickSelector,omitempty"`	
+	ClickType          string   `json:"clickType,omitempty"`	
+	ClickElementUnique string   `json:"clickElementUnique,omitempty"`
 }
 
 type login struct {
